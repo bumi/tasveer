6c86fc2a899a238adac23fbd1e89910272243fd3
Haik <haik.ampardjian@gmail.com>
2019-05-03 18:36:31 +0300

    Add All Collections scene UI

---
+++ Tasveer/Scenes/AllCollections/AllCollectionsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AllCollectionsViewController.swift
+//  Tasveer
+//
+//  Created by Haik Ampardjian on 5/3/19.
+//  Copyright © 2019 Haik Ampardjian. All rights reserved.
+//
+
+import UIKit
+import MaterialComponents
+
+final class AllCollectionsViewController: UIViewController {
+    private var dataSource: TableViewDataSource<AllCollectionsViewController>?
+    private var floatingButton: MDCFloatingButton?
+    private let queue = OperationQueue()
+    
+    @IBOutlet fileprivate weak var tableView: UITableView!
+}
+
+extension AllCollectionsViewController: TableViewDataSourceDelegate {
+    func configure(_ cell: AllCollectionsViewController.Cell, for object: AllCollectionsViewController.Object) {
+        
+    }
+    
+    func notifyEmptyData(isEmpty: Bool) {
+        
+    }
+}
