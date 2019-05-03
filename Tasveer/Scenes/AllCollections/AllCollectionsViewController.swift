29c0a18a93ab0376acc0e4df4750a5421362cd58
Haik <haik.ampardjian@gmail.com>
2019-05-03 19:57:23 +0300

    Add floating button to All Groups scene

--- Tasveer/Scenes/AllCollections/AllCollectionsViewController.swift
+++ Tasveer/Scenes/AllCollections/AllCollectionsViewController.swift
@@ -14,11 +14,47 @@ final class AllCollectionsViewController: UIViewController {
     private var floatingButton: MDCFloatingButton?
     private let queue = OperationQueue()
     
+    private let searchController = UISearchController(searchResultsController: nil)
+    
     @IBOutlet fileprivate weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+        setupFloatingButton()
+    }
+    
+    private func setupTableView() {
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 60.0
+        
+        tableView.register(AllCollectionCell.nib, forCellReuseIdentifier: AllCollectionCell.cellId)
+        
+        // FIXME: setup datasource
+    }
+    
+    private func setupFloatingButton() {
+        let plusImage = UIImage(named: "add")
+        floatingButton = MDCFloatingButton()
+        floatingButton?.setImage(plusImage, for: .normal)
+        floatingButton?.backgroundColor = UIColor.darkGray
+        floatingButton?.translatesAutoresizingMaskIntoConstraints = false
+        floatingButton?.addTarget(self, action: #selector(showCreateNewGroup), for: .touchUpInside)
+        
+        view.addSubview(floatingButton!)
+        
+        floatingButton?.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        floatingButton?.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+    }
+    
+    @objc private func showCreateNewGroup() {
+        
+    }
 }
 
 extension AllCollectionsViewController: TableViewDataSourceDelegate {
-    func configure(_ cell: AllCollectionCell, for object: AllCollectionsViewController.Object) {
+    func configure(_ cell: AllCollectionCell, for object: Group) {
         
     }
     
