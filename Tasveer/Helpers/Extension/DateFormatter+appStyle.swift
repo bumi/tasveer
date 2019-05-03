db45608947eb35d2e3e26f292ad9bd5362756b63
Haik <haik.ampardjian@gmail.com>
2019-05-03 18:05:20 +0300

    Add custom request response handler

---
+++ Tasveer/Helpers/Extension/DateFormatter+appStyle.swift
@@ -0,0 +1,21 @@
+//
+//  DateFormatter+appStyle.swift
+//  Tasveer
+//
+//  Created by Haik Ampardjian on 5/3/19.
+//  Copyright © 2019 Haik Ampardjian. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let iso8601: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        
+        return formatter
+    }()
+}
