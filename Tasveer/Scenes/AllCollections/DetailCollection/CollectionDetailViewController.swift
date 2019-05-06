589d44df296e7f5375d62d61f53000a58c6e1cdb
Haik <haik.ampardjian@gmail.com>
2019-05-06 11:53:34 +0300

    Add uiviewcontroller class for DetailsCollection scene

---
+++ Tasveer/Scenes/AllCollections/DetailCollection/CollectionDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CollectionDetailViewController.swift
+//  Tasveer
+//
+//  Created by Haik Ampardjian on 5/6/19.
+//  Copyright © 2019 Haik Ampardjian. All rights reserved.
+//
+
+import UIKit
+
+final class CollectionDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTopBar()
+    }
+    
+    private func setupTopBar() {
+        let filters = UIBarButtonItem.init(image: UIImage(named: "filter_icon"), style: UIBarButtonItem.Style.plain, target: self, action: #selector(openFilterScene))
+        navigationItem.rightBarButtonItem = filters
+    }
+    
+    @objc private func openFilterScene() {
+        
+    }
+}
