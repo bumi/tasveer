4c7342ccec170f66a4d1d90c46184236f58fde6e
Haik <haik.ampardjian@gmail.com>
2019-05-08 12:14:54 +0300

    Add Operation condition for authorization status

---
+++ Tasveer/Helpers/Extension/NSError+init.swift
@@ -0,0 +1,15 @@
+//
+//  NSError+init.swift
+//  Tasveer
+//
+//  Created by Haik Ampardjian on 5/8/19.
+//  Copyright © 2019 Haik Ampardjian. All rights reserved.
+//
+
+import Foundation
+
+extension NSError {
+    static func failed() {
+        
+    }
+}
