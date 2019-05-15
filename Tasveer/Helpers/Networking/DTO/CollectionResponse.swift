2eaef3542f3d2b37e6f2c555ac21e7eadfe61bc3
Haik <haik.ampardjian@gmail.com>
2019-05-15 16:07:34 +0300

    Update CollectionResponse struct when you fetch collection from networking request

---
+++ Tasveer/Helpers/Networking/DTO/CollectionResponse.swift
@@ -0,0 +1,27 @@
+//
+//  GroupResponse.swift
+//  Tasveer
+//
+//  Created by Haik Ampardjian on 5/3/19.
+//  Copyright © 2019 Haik Ampardjian. All rights reserved.
+//
+
+import Foundation
+
+struct CollectionResponse: Codable {
+    let name: String
+    let description: String?
+    let createdAt: Date
+    let identifier: String
+    let photos: [PhotoResponse]
+    let users: [UserResponse]
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case description
+        case createdAt = "created_at"
+        case identifier
+        case photos
+        case users
+    }
+}
