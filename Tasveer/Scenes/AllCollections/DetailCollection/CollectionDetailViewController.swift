616fd5809648cea29858b8556cb2a20a7e730a0f
Haik <haik.ampardjian@gmail.com>
2019-05-22 21:56:24 +0300

    Add test upload of first asset

--- Tasveer/Scenes/AllCollections/DetailCollection/CollectionDetailViewController.swift
+++ Tasveer/Scenes/AllCollections/DetailCollection/CollectionDetailViewController.swift
@@ -115,6 +115,9 @@ final class CollectionDetailViewController: UIViewController {
                 DispatchQueue.main.async {
                     if let fetchResult = operation.fetchResult {
                         self?.photos.fetchResult = fetchResult
+                        
+                        let op = UploadImageAssetToBackEndOperation(withPhoto: fetchResult.firstObject?.localIdentifier ?? "", parentProgress: Progress(totalUnitCount: 1))
+                        self?.queue.addOperation(op)
                     }
                 }
             }
