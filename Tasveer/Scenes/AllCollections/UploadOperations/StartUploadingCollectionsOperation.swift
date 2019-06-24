de3898e14ac5a5f2ed482f979adace576d30d9c1
Haik <haik.ampardjian@gmail.com>
2019-06-23 17:28:30 -0700

    Update the way new Upload operations are being add to the Group Operation

--- Tasveer/Scenes/AllCollections/UploadOperations/StartUploadingCollectionsOperation.swift
+++ Tasveer/Scenes/AllCollections/UploadOperations/StartUploadingCollectionsOperation.swift
@@ -9,11 +9,25 @@
 import Foundation
 
 final class StartUploadingCollectionsOperation: GroupOperation {
+    private let collections: [Collection]
+    
     init(withCollections collections: [Collection]) {
+        self.collections = collections
+        
+        super.init(operations: [])
+        
+        name = "Start Uploading Collections Operation"
+    }
+    
+    override func execute() {
         let operations = collections
             .filter({ $0.syncStateValue == .none || ($0.syncStateValue == .syncing && $0.task?.isPaused == true) })
             .map(StartUploadingCollectionOperation.init)
         
-        super.init(operations: operations)
+        for operation in operations {
+            addOperation(operation)
+        }
+        
+        super.execute()
     }
 }
